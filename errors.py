d72f25ca63812efe0dca1bf0ac9435a533b4511a
Kamil Wiackiewicz <wiackamil@gmail.com>
2026-07-12 16:52:32 +0200

    Wychwytywanie bledow

---
+++ errors.py
@@ -0,0 +1,15 @@
+def podaj(tekst):
+    while True:
+        try:
+            x = int(input(tekst))
+            if(x<0): 
+                raise ValueError()
+            return x
+        except ValueError:
+            print("Podaj sama liczbe!")
+czas = podaj("Podaj czas w minutach")
+tetno = podaj("Podaj tętno")
+print(czas*tetno*0.07)
+
+
+    
