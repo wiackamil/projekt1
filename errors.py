1871fe346c19ff7e4668e24c44ea030abe9d408f
Kamil Wiackiewicz <wiackamil@gmail.com>
2026-07-15 18:30:37 +0200

    Zapisywanie do pliku

--- errors.py
+++ errors.py
@@ -7,9 +7,22 @@ def podaj(tekst):
             return x
         except ValueError:
             print("Podaj sama liczbe!")
-czas = podaj("Podaj czas w minutach")
-tetno = podaj("Podaj tętno")
-print(czas*tetno*0.068)
+def licz_kalorie(czas, tetno):
+    return czas * tetno * 0.07
+class trening:
+    def __init__(self, nazwa):
+        self.nazwa = nazwa
+        self.czas = podaj("Podaj czas w minutach: ")
+        self.tetno = podaj("Podaj tętno: ")
+    def dopisz(self):
+        with open("dziennik.txt","a",encoding="utf-8") as plik:
+            plik.write(f"Trening: {self.nazwa} | Czas: {self.czas} min | Tetno: {self.tetno} | Kalorie: {licz_kalorie(self.czas, self.tetno)} \n")
+            print("trening zapisany w dzienniku!")
+def pokaz_historie():
+    print("Historia treningow: \n")
+    with open("dziennik.txt","r",encoding="utf-8") as plik:
+        print(plik.read())
 
-
-    
+wtorek = trening("bieganie")
+wtorek.dopisz()
+pokaz_historie()
