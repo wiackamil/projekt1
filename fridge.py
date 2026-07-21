40905fc6372d6356799f719bdf09e4c04c70e04e
Kamil Wiackiewicz <wiackamil@gmail.com>
2026-07-21 19:46:46 +0200

    naprawienie funkcji dodawania produktow

--- fridge.py
+++ fridge.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 
+
 class Produkt:
     def __init__(self,nazwa,ilosc,data,minIlosc):
         self.nazwa = nazwa
@@ -9,7 +10,13 @@ class Produkt:
         self.minIlosc = minIlosc
     def __repr__(self):
         return(f"{self.nazwa}, {self.ilosc} sztuki, {self.data}, alarm przy {self.minIlosc} sztukach")
-
+    def na_slownik(self):
+        return {
+            'nazwa':self.nazwa,
+            'ilosc':self.ilosc,
+            'data':self.data.strftime("%d.%m.%Y"),
+            'minIlosc':self.minIlosc
+        }
 def pobierz(rodzaj,tekst):
     if rodzaj == 'nazwa':
         return input(tekst)
@@ -31,11 +38,11 @@ def pobierz(rodzaj,tekst):
 
 
 def dodaj_produkt():
-    nazwa = pobierz('nazwa','Podaj nazwe swojego produktu')
-    ilosc = pobierz('liczba','Podaj ilosc produktu')
-    data = pobierz('data','Podaj date waznosci w formacie DD.MM.RRRR')
-    minIlosc = pobierz('liczba', 'Podaj minimalna ilosc przy jakiej ma wyskoczyc alert')
-    spizarka.add(Produkt(nazwa,ilosc,data,minIlosc))
+    nazwa = pobierz('nazwa','Podaj nazwe swojego produktu: ')
+    ilosc = pobierz('liczba','Podaj ilosc produktu: ')
+    data = pobierz('data','Podaj date waznosci w formacie DD.MM.RRRR: ')
+    minIlosc = pobierz('liczba', 'Podaj minimalna ilosc przy jakiej ma wyskoczyc alert: ')
+    spizarka.append(Produkt(nazwa,ilosc,data,minIlosc))
 def wypisz_menu():
     print(f"""
     ==============================
@@ -46,24 +53,63 @@ def wypisz_menu():
     4. Opuscic program
     """)
 
-spizarka = set()
+def wypisz_spizarka():
+    print('Stan spizarki: \n')
+    for i,przedmiot in enumerate(spizarka):
+        print(f"{i+1}. {przedmiot}")
 
 def wczytaj():
     try:
         with open("fridge_content.json","r",encoding="utf-8") as plik:
-            for product in plik:
-                spizarka.add(json.load(product))
-    except FileNotFoundError:
-        print("Błąd: Nie znaleziono pliku. najpierw cos zapisz")
+            dane = json.load(plik)
+            x = list()
+        for obiekt in dane:
+            produkt = Produkt(obiekt['nazwa'],obiekt['ilosc'],datetime.strptime(obiekt['data'], "%d.%m.%Y").date(),obiekt['minIlosc'])
+            x.append(produkt)
+        return x
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
 def zapisz():
     with open("fridge_content.json","w",encoding="utf-8") as plik:
-        json.dump(spizarka)
+        z = list()
+        for przedmiot in spizarka:
+            z.append(przedmiot.na_slownik())
+        json.dump(z,plik,indent=4)
 
-def usun(produkt):
-    pass
+def usun():
+    wypisz_spizarka()
+    try:
+        numer = pobierz('liczba','Wybierz numer produktu ktory chcesz usunac')-1
+        if numer>=len(spizarka) or numer<0:
+            raise Exception("Zly indeks produktu")          
+        ile = pobierz('liczba','Ile chcesz tego usunac?')
+        temp = spizarka[numer]
+        if temp.ilosc<ile:
+            raise Exception("Nie masz tyle w spizarce")
+        else:
+            if temp.ilosc-ile<0.01:
+                del spizarka[numer]
+            else:
+                spizarka[numer].ilosc-=ile
+    except Exception as e:
+        print(e)
+        return
 
-
-wypisz_menu()
-print(spizarka)
-dodaj_produkt()
+    
+spizarka = wczytaj()
+while True:
+    wypisz_menu()
+    wybor = input()
+    if wybor == '1':
+        wypisz_spizarka()
+    elif wybor == '2':
+        dodaj_produkt()
+    elif wybor == '3':
+        usun()
+    elif wybor == '4':
+        zapisz()
+        break
+    else:
+        print("blad, podaj jeszcze raz")
+exit()
